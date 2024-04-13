2f1941db16a497886692f6940826da175396d76c
Kai Kozlov <37962720+kaikozlov@users.noreply.github.com>
2024-04-12 22:13:29 -0500

    Add WE SLEEP

--- testing.py
+++ testing.py
@@ -10,17 +10,26 @@ duration = 10
 threshold = 60
 
 i=0
+vibrating=False
 
 # Callback function for the stream
 def audio_callback(indata, frames, time_info, status):
     global i
+    global vibrating
     volume_norm = np.linalg.norm(indata) * 10
     if volume_norm > threshold:
+        vibrating=True
         i += 1
-        print("Past!"+str(i))
+        print("LOUD ASF "+str(i))
+        for t in range(5, 0, -1):
+            print("LOUD SLEEPING "+str(t)+" more seconds")
+            time.sleep(1)  # sleep for 1 second
+    else:
+        vibrating=False
+        print("WE SLEEP 😴")
         for t in range(5, 0, -1):
             print("Sleeping "+str(t)+" more seconds")
-            time.sleep(1)  # sleep for 1 second
+            time.sleep(1)       
             
 # Open the stream
 stream = sd.InputStream(callback=audio_callback)
